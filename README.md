f3dcebab1c47c8e9d27ea827e48519eba84fad4a
Nikki Pham <nikkipham94@gmail.com>
2023-02-22 21:28:56 +1100

    removed repetitive win conditions

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ I started off by creating a wireframe of how I would like to structure my page a
 ![](README%20images/Screenshot%201%20Initial%20Planning%202023-02-22%20at%201.55.56%20pm.png)
 https://whimsical.com/W5MCxfLSh4HVPx8P7Ck63Vs
 
-Next I added functinality to my board such  that for each player 
+Next I added functinality to my board such  that for each player and hard coded to get a functional MVP. 
+
+Some issues from my initial wireframe was that I my draw message overwrote my span. An initial fix was to rewrite my message such that the player number was at the end but this required me to manually change the visibility of the player. 
+
+After my MVP was submitted- I removed the hard coded comment so that the comment can be adjusted in the javascript. 
--- app.js
+++ app.js
@@ -22,18 +22,16 @@ function handleClick(event) {
     numberOfTurns++
     console.log(numberOfTurns);
     //swap between players and swap between X and O
-        //? THINK OF A WAY TO MAKE THIS MORE READABLE AND REUSEABLE NOT USING IF STATEMENT BUT TRUE OR FALSE?
-    if (playerNumber.textContent === "1") {
+    if (numberOfTurns % 2 !== 0) {
         boxClicked.textContent = "X"
         playerNumber.textContent = 2
     } else {
         boxClicked.textContent = "O"
         playerNumber.textContent = 1 
     }
-    //so that you can only click box once 
-    boxClicked.removeEventListener("click", handleClick)
+    boxClicked.removeEventListener("click", handleClick)     //so that you can only click box once 
     //?  now to hard code the wins... first with X's
-    if (box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X") {
+    if ((box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X") || (box4.textContent === "X" && box5.textContent === "X" && box6.textContent === "X") || (box7.textContent === "X" && box8.textContent === "X" && box9.textContent === "X") || (box1.textContent === "X" && box4.textContent === "X" && box7.textContent === "X") || (box2.textContent === "X" && box5.textContent === "X" && box8.textContent === "X") || (box3.textContent === "X" && box6.textContent === "X" && box9.textContent === "X") || (box1.textContent === "X" && box5.textContent === "X" && box9.textContent === "X") || (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X")) {
         // make it such you can't click anymore boxes
         boxes.forEach(box => {
             box.removeEventListener("click", handleClick)
@@ -42,114 +40,14 @@ function handleClick(event) {
         resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
         gameCompletePopup.style.visibility = "visible"
         turnMessage.style.visibility = "hidden"
-    } else if (box4.textContent === "X" && box5.textContent === "X" && box6.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box7.textContent === "X" && box8.textContent === "X" && box9.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box1.textContent === "X" && box4.textContent === "X" && box7.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box2.textContent === "X" && box5.textContent === "X" && box8.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box3.textContent === "X" && box6.textContent === "X" && box9.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box1.textContent === "X" && box5.textContent === "X" && box9.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 1 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
         //? Now with O's
-    } else if (box1.textContent === "O" && box2.textContent === "O" && box3.textContent === "O") {
+    } else if ((box1.textContent === "O" && box2.textContent === "O" && box3.textContent === "O") || (box4.textContent === "O" && box5.textContent === "O" && box6.textContent === "O") || (box7.textContent === "O" && box8.textContent === "O" && box9.textContent === "O") || (box1.textContent === "O" && box4.textContent === "O" && box7.textContent === "O") || (box2.textContent === "O" && box5.textContent === "O" && box8.textContent === "O") || (box3.textContent === "O" && box6.textContent === "O" && box9.textContent === "O") || (box1.textContent === "O" && box5.textContent === "O" && box9.textContent === "O") || (box3.textContent === "O" && box5.textContent === "O" && box7.textContent === "O")) {
         boxes.forEach(box => {
             box.removeEventListener("click", handleClick)
         });
         resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
         gameCompletePopup.style.visibility = "visible"
         turnMessage.style.visibility = "hidden"
-    } else if (box4.textContent === "O" && box5.textContent === "O" && box6.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box7.textContent === "O" && box8.textContent === "O" && box9.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    }
-    else if (box1.textContent === "O" && box4.textContent === "O" && box7.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box2.textContent === "O" && box5.textContent === "O" && box8.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box3.textContent === "O" && box6.textContent === "O" && box9.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box1.textContent === "O" && box5.textContent === "O" && box9.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-    } else if (box3.textContent === "O" && box5.textContent === "O" && box7.textContent === "O") {
-        boxes.forEach(box => {
-            box.removeEventListener("click", handleClick)
-        });
-        resultsMessage.textContent = "Congratulations, Player 2 is the winner!"
-        gameCompletePopup.style.visibility = "visible"
-        turnMessage.style.visibility = "hidden"
-
     } else if (numberOfTurns === 9) { //FINALLY number of turns 
         resultsMessage.textContent = "Awww, it's a tie"
         gameCompletePopup.style.visibility = "visible"
@@ -174,5 +72,3 @@ function resetGame() {
 }
 
 playAgainBtn.addEventListener("click", resetGame)
-
-// Remove hard coded congrats so that won't have to toggle with visibility? 
