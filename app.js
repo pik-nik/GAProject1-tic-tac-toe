c35be9091aab03e71018b66d433f067c834f0fdd
Nikki Pham <nikkipham94@gmail.com>
2023-02-23 09:12:30 +1100

    trying arrays to shorten conditions in statement

--- app.js
+++ app.js
@@ -15,23 +15,72 @@ let resultsMessage = document.querySelector(".results-message")
 let playAgainBtn = document.querySelector(".play-again-button")
 
 let numberOfTurns = 0
+let waysToWin = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+let boxNumbersClickedByPlayer1 = []
+let boxNumbersClickedByPlayer2 = []
+
+let doesPlayer1win = false
+
+function checkIfPlayer1Wins(doesPlayer1win) {
+    waysToWin.forEach(winPattern => {
+        console.log('winPattern', winPattern);
+        winPattern.every(number => {
+            if (boxNumbersClickedByPlayer1.length >= 3 && boxNumbersClickedByPlayer1.includes(number) === true) {
+                console.log('potato', boxNumbersClickedByPlayer1.includes(number));
+            return boxNumbersClickedByPlayer1.includes(number)
+            // if true for all three numbers then return doesPlayer1win = true
+            }
+            
+        })
+        //if true turn doesPlayer1win into true
+    })
+} // this function returns true or false
+
+// function checkIfPlayer1Wins(doesPlayer1win) {
+//     while (doesPlayer1win = false) { 
+//         waysToWin[i].every(number => {
+//             doesPlayer1win = boxNumbersClickedByPlayer1.includes(number)
+//         })
+//         i++
+//     }
+// }
+
+
+
 function handleClick(event) {
-    console.log("clicking");
     let boxClicked = event.target
-    console.log(event.target);
+    console.log('event target', event.target);
     numberOfTurns++
-    console.log(numberOfTurns);
+    let boxNumberClicked = Number(boxClicked.dataset.num)
+    console.log('number of turns', numberOfTurns);
     //swap between players and swap between X and O
     if (numberOfTurns % 2 !== 0) {
         boxClicked.textContent = "X"
+        boxNumbersClickedByPlayer1.push(boxNumberClicked)
+        console.log('player 1', boxNumbersClickedByPlayer1);
         playerNumber.textContent = 2
     } else {
         boxClicked.textContent = "O"
+        boxNumbersClickedByPlayer2.push(boxNumberClicked)
+        console.log('player 2', boxNumbersClickedByPlayer2);
         playerNumber.textContent = 1 
     }
+    
     boxClicked.removeEventListener("click", handleClick)     //so that you can only click box once 
+
+    // let arr = [1, 2, 3]
+    // let arr2 = [1, 2, 3, 4]
+    // let check = arr.every(number => {
+    //     console.log('boxes clicked inside check', boxNumbersClickedByPlayer1);
+    //     return boxNumbersClickedByPlayer1.includes(number)
+    // })
+    // console.log(check);
+    console.log('does player 1 win?', checkIfPlayer1Wins(doesPlayer1win));
+
+
     //?  now to hard code the wins... first with X's
     if ((box1.textContent === "X" && box2.textContent === "X" && box3.textContent === "X") || (box4.textContent === "X" && box5.textContent === "X" && box6.textContent === "X") || (box7.textContent === "X" && box8.textContent === "X" && box9.textContent === "X") || (box1.textContent === "X" && box4.textContent === "X" && box7.textContent === "X") || (box2.textContent === "X" && box5.textContent === "X" && box8.textContent === "X") || (box3.textContent === "X" && box6.textContent === "X" && box9.textContent === "X") || (box1.textContent === "X" && box5.textContent === "X" && box9.textContent === "X") || (box3.textContent === "X" && box5.textContent === "X" && box7.textContent === "X")) {
+    // if (checkIfPlayer1Wins(doesPlayer1win)) {
         // make it such you can't click anymore boxes
         boxes.forEach(box => {
             box.removeEventListener("click", handleClick)
@@ -68,7 +117,10 @@ function resetGame() {
         numberOfTurns = 0
         playerNumber.textContent = 1 
         resultsMessage.textContent = "Congratulations! The winner is Player"
+        boxNumbersClickedByPlayer1 = []
+        boxNumbersClickedByPlayer2 = []
     });
 }
 
 playAgainBtn.addEventListener("click", resetGame)
+
